c2c945ca5670be7105968424ff148b66caa4f2e8
imasterit <imasterit117@gmail.com>
2020-04-30 10:59:28 +1000

    Finish the challenging task

--- backend/dashboard.py
+++ backend/dashboard.py
@@ -2,6 +2,7 @@ import pymongo
 
 from aiohttp import web
 from bson.objectid import ObjectId
+from json import JSONDecodeError
 
 from auth import auth_required
 
@@ -12,7 +13,22 @@ db = pymongo.MongoClient('db').moti
 @auth_required
 @routes.get('/dashboard/')
 async def dashboard(request):
-    """Placeholder for function that will generate the user's dashboard."""
     user_id = ObjectId(request.user)
-    name = db.users.find_one(user_id)['name']
-    return web.json_response({'name': name})
+    user = db.users.find_one(user_id)
+    name = user['name']
+    motto = user['motto'] if 'motto' in user else ''
+    return web.json_response({'name': name, 'motto': motto})
+
+
+@auth_required
+@routes.post('/dashboard/')
+async def update(request):
+    try:
+        post = await request.json()
+    except JSONDecodeError:
+        return web.json_response({'error': "Invalid JSON data."}, status=400)
+    motto = post['motto']
+    db.users.update_one(
+        {'_id': ObjectId(request.user)},
+        {'$set': {'motto': motto}})
+    return web.json_response({'motto': motto})
--- frontend/components/Dashboard.vue
+++ frontend/components/Dashboard.vue
@@ -2,6 +2,7 @@
   <div>
     <div>
       <p>{{message}}</p>
+      <input v-model="motto" @keyup="update" placeholder="Your motto" />
     </div>
   </div>
 </template>
@@ -11,7 +12,7 @@ import { authFetch } from './Auth.vue'
 
 export default {
   name: 'Dashboard',
-  data: () => ({ message: '' }),
+  data: () => ({ message: '', motto: '' }),
   created () { this.dashboard() },
   methods: {
     dashboard () {
@@ -22,8 +23,18 @@ export default {
             this.message = resp.error
           } else {
             this.message = resp.name
+            this.motto = resp.motto
           }
         })
+        .catch(err => console.log(err))
+    },
+    update () {
+      const opts = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application.json' },
+        body: JSON.stringify({ motto: this.motto })
+      }
+      authFetch('/api/dashboard/', opts)
         .catch(err => console.log(err))
     }
   }
